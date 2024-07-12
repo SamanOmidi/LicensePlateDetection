7485845191a5a9c8d26fb1ce9f394b1199387c50
SamanOmidi <samanomidi55@gmail.com>
2024-07-12 17:01:05 +0330

    letters division updated

--- src/letters.py
+++ src/letters.py
@@ -1,28 +1,31 @@
 import cv2
 import numpy as np
 import keras
+import imutils
 
-image_src = "output/plate.png"
+image_src = "output/detected_plate.jpg"
 
 image = cv2.imread(image_src)
+image = imutils.resize(image, 1300, 1300)
+
 
 height, width = image.shape[:2]
 
-h1 = 90
+h1 = 50
 h2 = height - 50
 
 # First number
-first_number = image[h1:h2, 175:210]
+first_number = image[h1:h2, 170:290]
 # Second number
-second_number = image[h1:h2, 210:250]
+second_number = image[h1:h2, 290:430]
 # Letter
-letter = image[h1:h2, 250:315]
+letter = image[h1:h2, 430:615]
 # Third number
-third_number = image[h1:h2, 315:355]
+third_number = image[h1:h2, 615:760]
 # Fourth number
-fourth_number = image[h1:h2, 355:390]
+fourth_number = image[h1:h2, 760:870]
 # Fifth number
-fifth_number = image[h1:h2, 390:430]
+fifth_number = image[h1:h2, 870:1000]
 
 # Load model
 model = keras.models.load_model("models/full_model.h5")
@@ -35,8 +38,8 @@ classes = ["0", "1", "2", "3", "4", "5", "6",
 l = [first_number, second_number, letter, third_number, fourth_number, fifth_number]
 plate = ""
 for img in l:
-    # cv2.imshow("img", img)
-    # cv2.waitKey(0)
+    cv2.imshow("img", img)
+    cv2.waitKey(0)
     cv2.imwrite("letters/letter.jpg", img)
     img = keras.preprocessing.image.load_img("letters/letter.jpg", target_size=(64, 64), color_mode="grayscale")
     img_array = keras.preprocessing.image.img_to_array(img) / 255.0
